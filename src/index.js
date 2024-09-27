fd4b54a67f8353c6ac63b0fa936bf664e35f7038
HÁ <haidee.alvarez.c@gmail.com>
2024-09-27 16:04:00 +0100

    design html structure

---
+++ src/index.js
@@ -0,0 +1,5 @@
+// click the button to generate a pretty bad dad's joke
+// when button chick:
+// welcome message desappear until page is refreshed
+// a joke appears instead 
+// every click new bad joke appears
