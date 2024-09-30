ad6c30298117ee159e0adbd1a5021a3009731d33
HÁ <haidee.alvarez.c@gmail.com>
2024-09-30 10:57:44 +0100

    styled

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
-// click the button to generate a pretty bad dad's joke
+function generate_joke() {
+  key = "9453eocfb302f861c59f1e9f04d3bta4";
+  promt_ = "Generate a joke";
+  context =
+    "You are a professional comedian that specialices on short jokes, your background is dark comedy";
+  apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt_}&context=${context}&key=${key}`;
+  let welcomeText = document.querySelector("#welcome-message");
+  welcomeText.classList.remove(
+    "Click the button above to get a pretty bad joke!"
+  );
+}
 // when button chick:
 // welcome message desappear until page is refreshed
-// a joke appears instead 
+// a joke appears instead
 // every click new bad joke appears
+// click the button to generate a pretty bad dad's joke
+let button = document.querySelector("#button");
+button.addEventListener("click", generate_joke);
