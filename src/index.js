2628f0eb7048f926e46a9632132bef1d6ce10e68
HÁ <haidee.alvarez.c@gmail.com>
2024-10-01 09:40:08 +0100

    updated context for AI joke

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ function generateJoke() {
   theKey = "9453eocfb302f861c59f1e9f04d3bta4";
   prompt_ = "Generate a joke";
   context =
-    "You are a professional comedian that specialices on short jokes, you know all the joke in the world";
+    "You are a professional comedian that specialices on jokes. You choose between these themes: party, love, animals, dark comedy or people jokes. You use the appropiate emojis";
   apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt_}&context=${context}&key=${theKey}`;
   axios.get(apiUrl).then(showAnswer);
 }
