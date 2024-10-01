7606fda36f4f1c04401c627a11e3bc6a3b42b6c0
HÁ <haidee.alvarez.c@gmail.com>
2024-10-01 10:49:44 +0100

    added typewriter effect with a plugin

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 function showAnswer(response) {
   let joke = document.querySelector("#welcome-message");
-  joke.innerHTML = response.data.answer;
+
+  new Typewriter("#welcome-message", {
+    strings: response.data.answer,
+    pauseFor: 1000,
+    autoStart: true,
+    cursor: null,
+    delay: 50,
+  });
+  //joke.innerHTML = response.data.answer;
 }
 
 function generateJoke() {
   theKey = "9453eocfb302f861c59f1e9f04d3bta4";
   prompt_ = "Generate a joke";
   context =
-    "You are a professional comedian that specialices on jokes. You choose between these themes: party, love, animals, dark comedy or people jokes. You use the appropiate emojis";
+    "You are a professional comedian that specialices on jokes. You choose between these themes: party, love, animals, cheese, dark comedy or people jokes. You use the appropiate emojis";
   apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt_}&context=${context}&key=${theKey}`;
   axios.get(apiUrl).then(showAnswer);
 }
