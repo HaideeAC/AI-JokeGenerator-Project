a1c62b25767a830cf6da3d917c89d9a2956782a9
HÁ <haidee.alvarez.c@gmail.com>
2024-11-15 15:21:42 +0000

    shadow

--- src/index.js
+++ src/index.js
@@ -1,20 +1,13 @@
 function showAnswer(response) {
   let joke = document.querySelector("#welcome-message");
-
-  new Typewriter("#welcome-message", {
-    strings: response.data.answer,
-    pauseFor: 1000,
-    autoStart: true,
-    cursor: null,
-    delay: 50,
-  });
+  joke.innerHTML = response.data.answer;
 }
 
 function generateJoke() {
-  theKey = "9453eocfb302f861c59f1e9f04d3bta4";
-  prompt_ = "Generate a joke";
-  context =
-    "You are a professional comedian that specialices on jokes. You choose between these themes: party, love, animals, cheese, dark comedy or people jokes. You use the appropiate emojis";
+  let theKey = "9453eocfb302f861c59f1e9f04d3bta4";
+  let prompt_ = "Generate a joke";
+  let context =
+    "You are a professional comedian that specialices on short jokes, you know all the joke in the world";
   apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt_}&context=${context}&key=${theKey}`;
   axios.get(apiUrl).then(showAnswer);
 }
